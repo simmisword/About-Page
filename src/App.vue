9feb21737506a5f4f64862c010de97cb0d72ec3a
simmisword <simon-schwertner@t-online.de>
2020-11-19 03:14:47 +0100

    firebase hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,31 @@
   </section>
 </template>
 
+
+<script src="https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js"></script>
+
+
+<script src="https://www.gstatic.com/firebasejs/8.0.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDFj1H1qSvLZgkWAoSwJE9JZQvVvvTOkOw",
+    authDomain: "siggifunkl.firebaseapp.com",
+    databaseURL: "https://siggifunkl.firebaseio.com",
+    projectId: "siggifunkl",
+    storageBucket: "siggifunkl.appspot.com",
+    messagingSenderId: "816797410586",
+    appId: "1:816797410586:web:4cc578a8b55994fa42f8a3",
+    measurementId: "G-7RWGEFLWY3"
+  };
+  // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
+</script>
+
+
 <script lang="ts">
 import Vue from 'vue'
 import MenuBar from "@/components/MenuBar.vue"
